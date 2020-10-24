bf13542baa222a920885ebbb4d0e9131a5ceeaba
Chris Martin <ch.martin@gmail.com>
2020-10-24 16:08:31 -0600

    build links dir

--- lib/BuildAll.hs
+++ lib/BuildAll.hs
@@ -1,5 +1,7 @@
 module BuildAll where
 
+import Path
+import Path.IO
 import Pipes
 import qualified Pipes.Prelude as Pipes
 import Relude
@@ -8,4 +10,19 @@ import ResourcePaths
 import Scheme
 
 main :: IO ()
-main = runEffect $ findProHtmlResources scheme >-> Pipes.mapM_ (buildResource putStrLn)
+main =
+  do
+    runEffect $
+        findProHtmlResources scheme
+        >-> Pipes.mapM_ (buildResource putStrLn)
+    runEffect $
+        findProHtmlResources scheme
+        >-> Pipes.mapM_ linkResource
+
+linkResource :: Resource -> IO ()
+linkResource r =
+  for_ (resourceOutputPath scheme r) \p ->
+    do
+      let p' = [reldir|links|] </> p
+      createDirIfMissing True (parent p')
+      createFileLink p p'
