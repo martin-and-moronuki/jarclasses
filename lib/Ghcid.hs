995f66d6083f163b73bd911c2b9c8c2ccc52847b
Chris Martin <ch.martin@gmail.com>
2020-10-24 16:40:25 -0600

    ghcid.txt

--- lib/Ghcid.hs
+++ lib/Ghcid.hs
@@ -4,4 +4,4 @@ import Prelude
 import System.Process
 
 main :: IO ()
-main = callProcess "ghcid" ["--command=runhaskell -ilib RunHaskell GhciInNixShell -ignore-dot-ghci", "--color=always", "--ignore-loaded", "--run=Run.main", "--warnings", "--reload=lib", "--restart=shell.nix"]
+main = callProcess "ghcid" ["--command=runhaskell -ilib RunHaskell GhciInNixShell -ignore-dot-ghci", "--outputfile=ghcid.txt", "--color=always", "--ignore-loaded", "--run=Run.main", "--warnings", "--reload=lib", "--restart=shell.nix"]
