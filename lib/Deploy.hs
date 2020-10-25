390039e578cd25b936a71035b3ffc8507e2d7550
Chris Martin <ch.martin@gmail.com>
2020-10-24 18:02:20 -0600

    build before deploy

--- lib/Deploy.hs
+++ lib/Deploy.hs
@@ -2,6 +2,10 @@ module Deploy where
 
 import Relude
 import System.Process
+import qualified BuildAll
 
 main :: IO ()
-main = callProcess "rsync" ["--recursive", "--progress", "--human-readable", "--verbose", "--compress", "--rsh=ssh -p 36411", "all/", "chris-martin.org:/var/www/jarclasses.com/"]
+main = BuildAll.main *> deploy
+
+deploy :: IO ()
+deploy = callProcess "rsync" ["--recursive", "--progress", "--human-readable", "--verbose", "--compress", "--rsh=ssh -p 36411", "all/", "chris-martin.org:/var/www/jarclasses.com/"]
