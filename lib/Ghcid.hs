39c8e96f70a6b17892bc0bfd6bc2e195660f67eb
Chris Martin <ch.martin@gmail.com>
2020-10-24 18:45:02 -0600

    consolidate ghci runners into Haskell module, don't use runhaskell

--- lib/Ghcid.hs
+++
@@ -1,7 +0,0 @@
-module Ghcid where
-
-import System.Process
-import Prelude
-
-main :: IO ()
-main = callProcess "ghcid" ["--command=runhaskell -ilib RunHaskell Ghci mainForGhcidInNixShell", "--outputfile=ghcid.txt", "--color=always", "--ignore-loaded", "--run=Run.main", "--warnings", "--reload=lib", "--restart=shell.nix", "--restart=lib/Haskell.hs"]
--- lib/Haskell.hs
+++ lib/Haskell.hs
@@ -1,6 +1,11 @@
+{-# LANGUAGE QuasiQuotes #-}
+
 module Haskell where
 
+import Path
+import Path.IO
 import Relude
+import System.Process
 
 languageExtensions :: [String]
 languageExtensions =
@@ -18,3 +23,41 @@ languageExtensions =
 
 extensionFlags :: [String]
 extensionFlags = map ("-X" <>) languageExtensions
+
+run :: String -> String -> IO ()
+run x y = callProcess "ghc" (["-ilib"] <> extensionFlags <> [x, "-e", y])
+
+ghci :: IO ()
+ghci =
+  getTargets >>= \targets ->
+    callProcess "ghci" (["-Wall", "-fdefer-typed-holes", "-ilib", "-ferror-spans", "-fdiagnostics-color=always"] <> extensionFlags <> targets)
+
+ghcid :: IO ()
+ghcid =
+  callProcess "ghcid" ["--command=ghc -ilib Haskell -e mainForGhcidInNixShell", "--outputfile=ghcid.txt", "--color=always", "--ignore-loaded", "--run=Run.main", "--warnings", "--reload=lib", "--restart=shell.nix", "--restart=lib/Haskell.hs"]
+
+mainForGhcidInNixShell :: IO ()
+mainForGhcidInNixShell =
+  callProcess "nix-shell" ["shell.nix", "--pure", "--command", "ghc -ilib Haskell -e mainForGhcid"]
+
+mainForGhcid :: IO ()
+mainForGhcid =
+  getTargets >>= \targets ->
+    callProcess "ghci" (["-Wall", "-fdefer-typed-holes", "-ilib", "-ferror-spans", "-fdiagnostics-color=always", "-ignore-dot-ghci"] <> extensionFlags <> targets)
+
+getTargets :: IO [String]
+getTargets =
+  fmap (\(_, xs) -> mapMaybe pathModule xs) $
+    listDirRecurRel [reldir|lib|]
+
+pathModule :: Path Rel File -> Maybe String
+pathModule x =
+  case splitExtension x of
+    Just (y, ".hs") -> Just $ map slashToDot $ toFilePath y
+    _ -> Nothing
+
+slashToDot :: Char -> Char
+slashToDot c =
+  case c of
+    '/' -> '.'
+    _ -> c
