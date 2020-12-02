622c46a688e446ec484ff995fb326fc542d28e8b
Chris Martin <ch.martin@gmail.com>
2020-12-01 23:34:46 -0700

    format

--- lib/Resource.hs
+++ lib/Resource.hs
@@ -1,34 +1,37 @@
 module Resource where
 
 import Data.Data (Data)
-import Text.Show
+import qualified Data.Text as T
 import Language.Haskell.TH.Quote
 import Language.Haskell.TH.Syntax
 import Relude
-import qualified Data.Text as T
+import Text.Show
 
 -- A "resource" is anything that a client might request from our web server, such as an HTML page, a CSS stylesheet, or an image.
 newtype Resource = ResourceSlashList [Text]
   deriving stock (Eq, Ord, Generic, Data)
   deriving anyclass (Hashable)
 
-instance Show Resource
-  where
-    showsPrec _ (ResourceSlashList xs) =
-      showString "[res|" .
-      showString (toString (T.intercalate "/" xs)) .
-      showString "|]"
+instance Show Resource where
+  showsPrec _ (ResourceSlashList xs) =
+    showString "[res|"
+      . showString (toString (T.intercalate "/" xs))
+      . showString "|]"
 
 resourceExp :: Resource -> Q Exp
 resourceExp (ResourceSlashList xs) =
-  [e| ResourceSlashList $
-        map (toText :: String -> Text)
-            $(liftData $ map toString xs) |]
+  [e|
+    ResourceSlashList $
+      map
+        (toText :: String -> Text)
+        $(liftData $ map toString xs)
+    |]
 
 res :: QuasiQuoter
-res = QuasiQuoter
-  { quoteExp = resourceExp . ResourceSlashList . filter (not . T.null) . T.splitOn "/" . toText
-  , quotePat = const $ fail "Cannot be used in a pattern context."
-  , quoteType = const $ fail "Cannot be used in a type context."
-  , quoteDec = const $ fail "Cannot be used in a declaration context."
-  }
+res =
+  QuasiQuoter
+    { quoteExp = resourceExp . ResourceSlashList . filter (not . T.null) . T.splitOn "/" . toText,
+      quotePat = const $ fail "Cannot be used in a pattern context.",
+      quoteType = const $ fail "Cannot be used in a type context.",
+      quoteDec = const $ fail "Cannot be used in a declaration context."
+    }
