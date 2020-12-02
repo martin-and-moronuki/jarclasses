64e19fcfe2db2288e8ef6dc706eea61abfc53969
Chris Martin <ch.martin@gmail.com>
2020-12-02 14:21:51 -0700

    fiddle with css

--- style/Style.hs
+++ style/Style.hs
@@ -1,6 +1,7 @@
 module Style where
 
-import Clay
+import Clay hiding (bold, italic)
+import qualified Clay
 import Path
 import Pipes
 import Relude hiding ((&))
@@ -16,15 +17,10 @@ makeStyles d = writeFileLBS path (encodeUtf8 txt)
     path = Path.toFilePath (d Path.</> [relfile|style/jarclasses.css|])
     txt = renderWith pretty [] jarclassesStyle
 
-contentWidthPx :: Double
-contentWidthPx = 650
-
 jarclassesStyle :: Css
 jarclassesStyle =
   do
-    p ? do
-      marginAll (px 0)
-      paddingAll (px 0)
+    p ? (noMargin <> noPadding)
     body ? background (rgb 0xf0 0xe8 0xe8)
     main_ ? do
       marginVertical (px 80)
@@ -37,38 +33,37 @@ jarclassesStyle =
         lineHeight (unitless 1.6)
         textAlign justify
         contentFontFamily
-        foldMap1 (\cls -> span # byClass cls) ("emphatic" :| "title" : []) ? fontStyle italic
-        a ? do
-          textDecoration none
-          color (rgb 0x41 0x70 0x90)
-          hover & textDecoration underline
+        foldMap1 (\cls -> span # byClass cls) ("emphatic" :| "title" : []) ? italic
+        a ? (textDecoration none <> color (rgb 0x41 0x70 0x90) <> hoverUnderline)
       p <? marginVertical (em 0.7)
-      headerTags <? do
-        headerFontFamily
-        fontWeight bold
+      headerTags <? (headerFontFamily <> bold)
       (h1 <> h2) <? do
         color (rgb 0x7c 0x33 0x4f)
         borderBottomColor (rgb 0xd3 0xcc 0xc1)
         lineHeight (unitless 1.2)
         paddingBottom (px 10)
       h1 <? do
-        fontSize (em 1.85)
-        fontStyle italic
-        textAlign center
-        marginTop (em 1.1)
-        marginBottom (em 1.5)
-        borderBottomStyle double
-        borderBottomWidth (em 0.2)
+        fontSize (em 1.85) <> italic <> textAlign center
+        marginTop (em 1.1) <> marginBottom (em 1.5)
+        borderBottomStyle double <> borderBottomWidth (em 0.2)
       h2 <? do
         fontSize (em 1.58)
-        marginTop (em 2)
-        marginBottom (em 0.5)
-        borderBottomStyle solid
-        borderBottomWidth (em 0.1)
+        marginTop (em 2) <> marginBottom (em 0.5)
+        borderBottomStyle solid <> borderBottomWidth (em 0.1)
+
+contentWidthPx :: Double
+contentWidthPx = 650
 
 contentFontFamily, headerFontFamily :: Css
 headerFontFamily = fontFamily ["Open Sans", "Myriad", "Calibri"] [sansSerif]
 contentFontFamily = fontFamily ["Georgia", "Palatino", "Palatino Linotype", "Times", "Times New Roman"] [serif]
+
+noMargin, noPadding, hoverUnderline, bold, italic :: Css
+noMargin = marginAll (px 0)
+noPadding = paddingAll (px 0)
+hoverUnderline = hover & textDecoration underline
+bold = fontWeight Clay.bold
+italic = fontStyle Clay.italic
 
 marginAll, marginVertical, marginHorizontal, paddingAll, paddingVertical, paddingHorizontal :: Size a -> Css
 marginAll = marginVertical <> marginHorizontal
