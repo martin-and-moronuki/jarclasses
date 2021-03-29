32bc6866e116d46dab8ad3d8553c29bf1c3a4acd
Chris Martin <ch.martin@gmail.com>
2021-03-29 02:49:22 -0600

    update regex-applicative to latest

--- haskell.nix
+++ haskell.nix
@@ -4,5 +4,11 @@ let
     hsPackageSelection = haskellPackages: with haskellPackages; [
         aeson aeson-optics aeson-pretty appendmap ascii-th async bytestring clay either-list-functions fsnotify http-conduit lens optics ormolu path path-io pipes prosidy relude regex-applicative safe-exceptions stm stm-containers text unordered-containers validation wai warp
     ];
+
+    hsOverrides = self: super: {
+        regex-applicative = self.callHackage "regex-applicative" "0.3.4" {};
+    };
+
+    hsPackages = pkgs.haskellPackages.override { overrides = hsOverrides; };
 in
-    pkgs.haskellPackages.ghcWithPackages hsPackageSelection
+    hsPackages.ghcWithPackages hsPackageSelection
